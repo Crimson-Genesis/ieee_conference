a428c76221ca088fd8d80e22c8569c8a9f9388ab
Atomic <nico.zero.0x@gmail.com>
2025-01-12 18:38:04 +0530

    catching up to the main (#11)
    * Spine squash to main. (#1)
    
    * change in the README.md
    
    * change in README.md
    
    * add static site (#3)
    
    * add static site
    
    * Spine squash to main. (#1) (#2)
    
    * change in the README.md
    
    * change in README.md
    
    ---------
    
    Co-authored-by: Atomic <nico.zero.0x@gmail.com>
    
    * Spine squash to main. (#5)
    
    * change in the README.md
    
    * change in README.md
    
    * from main to spine (#4)
    
    * Spine squash to main. (#1)
    
    * change in the README.md
    
    * change in README.md
    
    * add static site (#3)
    
    * add static site
    
    * Spine squash to main. (#1) (#2)
    
    * change in the README.md
    
    * change in README.md
    
    ---------
    
    Co-authored-by: Atomic <nico.zero.0x@gmail.com>
    
    ---------
    
    Co-authored-by: Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
    
    * added the django spine...
    
    ---------
    
    Co-authored-by: Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
    
    * Removed the metadata from the spine and squashing to main. (#6)
    
    * change in the README.md
    
    * change in README.md
    
    * from main to spine (#4)
    
    * Spine squash to main. (#1)
    
    * change in the README.md
    
    * change in README.md
    
    * add static site (#3)
    
    * add static site
    
    * Spine squash to main. (#1) (#2)
    
    * change in the README.md
    
    * change in README.md
    
    ---------
    
    Co-authored-by: Atomic <nico.zero.0x@gmail.com>
    
    ---------
    
    Co-authored-by: Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
    
    * added the django spine...
    
    * deleted the matadata
    
    * deleted the migration and __pycech__...
    
    * added the .gitignore...
    
    * deleted the database...
    
    * update....
    
    ---------
    
    Co-authored-by: Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
    
    * last mistake (#7)
    
    * change in the README.md
    
    * change in README.md
    
    * from main to spine (#4)
    
    * Spine squash to main. (#1)
    
    * change in the README.md
    
    * change in README.md
    
    * add static site (#3)
    
    * add static site
    
    * Spine squash to main. (#1) (#2)
    
    * change in the README.md
    
    * change in README.md
    
    ---------
    
    Co-authored-by: Atomic <nico.zero.0x@gmail.com>
    
    ---------
    
    Co-authored-by: Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
    
    * added the django spine...
    
    * deleted the matadata
    
    * deleted the migration and __pycech__...
    
    * added the .gitignore...
    
    * deleted the database...
    
    * update....
    
    * last mistake...
    
    ---------
    
    Co-authored-by: Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>
    
    * Delete .gitignore
    
    * Delete db.sqlite3
    
    * Delete main/__pycache__ directory
    
    * Delete main/migrations directory
    
    * Delete pages/__pycache__ directory
    
    * Delete pages/migrations directory
    
    * Delete reva_ieee_conference/__pycache__ directory
    
    * Spine (#8)
    
    * Updates (#9)
    
    * add static site
    
    * Spine squash to main. (#1) (#2)
    
    * change in the README.md
    
    * change in README.md
    
    * add logo in footer
    
    * added color scheme and fixed navbar
    
    ---------
    
    Co-authored-by: Atomic <nico.zero.0x@gmail.com>
    
    ---------
    
    Co-authored-by: Aryan Arkhit Behra <93533363+notarkhit@users.noreply.github.com>

--- pages/views.py
+++ pages/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(*args, **kwargs):
-    return HttpResponse("<h1>This is a Landing page.</h1>")  # type: ignore
+    return HttpResponse("<h1>This is a Landing page.</h1>") # type: ignore
 
 
 def about_view(*args, **kwargs):
-    return HttpResponse("<h1>This is a about page.</h1>")  # type: ignore
+    return HttpResponse("<h1>This is a Landing page.</h1>")  # type: ignore

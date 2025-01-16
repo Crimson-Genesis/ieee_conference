2fa623a7a012e5d1beea1df2318dc6c847a182a7
Crimson-Genesis <nico.zero.0x@gmail.com>
2025-01-16 10:39:22 +0530

    update...

--- main/static/js/scripts.js
+++ main/static/js/scripts.js
@@ -1,7 +1,7 @@
 // JavaScript code to generate bubbles
  document.addEventListener("DOMContentLoaded", function() {
   const bubblesContainer = document.querySelector('.bubbles');
-  const bubbleCount = 128;
+  const bubbleCount = 500;
   
   for (let i = 0; i < bubbleCount; i++) {
     const bubble = document.createElement('div');
@@ -9,7 +9,7 @@
     
     // Set random CSS variables for each bubble
     bubble.style.setProperty('--size', `${2 + Math.random() * 4}rem`);
-    bubble.style.setProperty('--distance', `${6 + Math.random() * 4}rem`);
+    bubble.style.setProperty('--distance', `${3 + Math.random() * 1}rem`);
     bubble.style.setProperty('--position', `${-5 + Math.random() * 110}%`);
     bubble.style.setProperty('--time', `${2 + Math.random() * 2}s`);
     bubble.style.setProperty('--delay', `${-(2 + Math.random() * 2)}s`);
